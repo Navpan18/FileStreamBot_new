651e359efb96e017047f36eee1f70a41e7e05583
Navneet Panchayan <navneet.panchayan@bitdurg.ac.in>
2023-12-16 16:09:38 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Telegram:
-    API_ID = int(env.get("API_ID"))
-    API_HASH = str(env.get("API_HASH"))
-    BOT_TOKEN = str(env.get("BOT_TOKEN"))
-    OWNER_ID = int(env.get('OWNER_ID', '7978482443'))
+    API_ID = int('20295350')
+    API_HASH = str('805a0a86f3b382d904617d0a2fd4fb6f')
+    BOT_TOKEN = str('6860327887:AAGTxyHUBY3F9nZvgC0rs0F0jSD8GqvI2r8')
+    OWNER_ID = int('1220356966')
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    DATABASE_URL = str(env.get('DATABASE_URL'))
+    DATABASE_URL = str('mongodb+srv://Navpan:Ramnagar@telegrambot2.5aga6dx.mongodb.net/?retryWrites=true&w=majority')
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
     FORCE_UPDATES_CHANNEL = env.get('FORCE_UPDATES_CHANNEL', False)
@@ -17,9 +17,7 @@ class Telegram:
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
     IMAGE_FILEID = env.get('IMAGE_FILEID', "https://telegra.ph/file/5bb9935be0229adf98b73.jpg")
     MULTI_CLIENT = False
-    LOG_CHANNEL = int(
-        env.get("BIN_CHANNEL", None)
-    )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
+    LOG_CHANNEL = int('-1002107974721')
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
